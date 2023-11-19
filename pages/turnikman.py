1574c6f20e4f307f6b64feea092d95e4d82fa708
Firuz Juraev <62535926+Firuz-JuraevML@users.noreply.github.com>
2023-11-19 21:51:46 +0500

    Update turnikman.py

--- pages/turnikman.py
+++ pages/turnikman.py
@@ -23,19 +23,21 @@ def main():
    
   st.title(f"🏋‍♂️ Turnikman") 
  
-  col1, col2, col3, col4 = st.columns((3, 2, 2, 2)) 
+  col1, col2, col3, col4, col5 = st.columns((3, 2, 2, 2, 2)) 
   col1.write("**Date**") 
   col2.write("**Max**") 
   col3.write("**Total**") 
+  col3.write("**Type**")
   col4.write("**Record**") 
 
   
   for i in range(data.shape[0]): 
-    col1, col2, col3, col4 = st.columns((3, 2, 2, 2)) 
+    col1, col2, col3, col4, col5 = st.columns((3, 2, 2, 2, 2)) 
     col1.write(f"📆 {data.iloc[i].sana}") 
     col2.write(data.iloc[i].maximal)
     col3.write(data.iloc[i].total)
-    col4.write(data.iloc[i].record)
+    col4.write(data.iloc[i].type)
+    col5.write(data.iloc[i].record)
       
 
 if __name__ == '__main__':
