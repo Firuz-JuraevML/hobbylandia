0030470d75b1489a75ccdaa180325774811c7c22
Firuz Juraev <62535926+Firuz-JuraevML@users.noreply.github.com>
2024-01-03 19:22:33 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
 import streamlit as st 
+import pandas as pd 
+from datetime import datetime 
 
-st.title("Discipline = Freedom") 
+def read_data():  
+  data_link = "https://docs.google.com/spreadsheets/d/e/2PACX-1vREos55t0BcKIWlqD3_YTX4OnN2RX2_qQhYqUJL2cB9aRL3DUM_X2BCknrySUYa3fFG-khR1R2Mbk57/pub?gid=231521276&single=true&output=csv" 
+  data = pd.read_csv(data_link) 
+
+  return data  
+  
+
+def main():  
+  st.title("Discipline = Freedom")   
+  data = read_data() 
+
+
+
+if __name__ == '__main__':
+    main()
