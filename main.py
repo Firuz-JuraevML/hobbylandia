990f09e9006eb05f21f0d14f3fb9acb682dfe0f7
Firuz Juraev <62535926+Firuz-JuraevML@users.noreply.github.com>
2024-09-09 11:34:06 +0500

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def main():
   st.title("Discipline = Freedom")   
   data = read_data() 
 
-  start_date = datetime.strptime("2024-01-04", "%Y-%m-%d").date() 
+  start_date = datetime.strptime("2025-03-31", "%Y-%m-%d").date() 
   current_date = datetime.now().date()
   difference_in_days = (current_date - start_date).days  
 
